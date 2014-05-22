d0f91ee2b7482d3a8e87eed9204fb5fe62d56b49
Ilton Garcia <ilton_unb@hotmail.com>
2014-05-22 13:56:21 -0300

    base View proj

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> GoogleMaps API </title>
+		<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+		<script type="text/javascript" src='app.js'></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	</head>
+	<body>
+
+
+
+	</body>
+</html>
